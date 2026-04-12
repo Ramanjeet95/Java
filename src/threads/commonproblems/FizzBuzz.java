c37be82b683f24d4efe00676d5915b8e8cf3ac53
Raman <ramanjeetsingh97@yahoo.in>
2026-04-12 23:48:01 +0200

    FizzBuzz with 4 threads

---
+++ src/threads/commonproblems/FizzBuzz.java
@@ -0,0 +1,126 @@
+package threads.commonproblems;
+
+import java.util.concurrent.Semaphore;
+import java.util.function.IntConsumer;
+
+class FizzBuzz {
+    private int n;
+    private volatile boolean isEnded = false;
+    private final Semaphore fizzSemaphore = new Semaphore(0);
+    private final Semaphore buzzSemaphore = new Semaphore(0);
+    private final Semaphore fizzBuzzSemaphore = new Semaphore(0);
+    private final Semaphore numberSemaphore = new Semaphore(1);
+
+    public FizzBuzz(int n) {
+        this.n = n;
+    }
+
+    // printFizz.run() outputs "fizz".
+    public void fizz(Runnable printFizz) throws InterruptedException {
+        try {
+            while(!isEnded) {
+                fizzSemaphore.acquire();
+                if(isEnded) {
+                    return;
+                }
+                printFizz.run();
+                numberSemaphore.release();
+            }
+        } catch(InterruptedException e) {
+            Thread.currentThread().interrupt();
+        }
+    }
+
+    // printBuzz.run() outputs "buzz".
+    public void buzz(Runnable printBuzz) throws InterruptedException {
+        try {
+            while(!isEnded) {
+                buzzSemaphore.acquire();
+                if(isEnded) {
+                    return;
+                }
+                printBuzz.run();
+                numberSemaphore.release();
+            }
+
+        } catch(InterruptedException e) {
+            Thread.currentThread().interrupt();
+        }
+    }
+
+    // printFizzBuzz.run() outputs "fizzbuzz".
+    public void fizzbuzz(Runnable printFizzBuzz) throws InterruptedException {
+        try {
+            while(!isEnded) {
+                fizzBuzzSemaphore.acquire();
+                if(isEnded) {
+                    return;
+                }
+                printFizzBuzz.run();
+                numberSemaphore.release();
+            }
+        } catch(InterruptedException e) {
+            Thread.currentThread().interrupt();
+        }
+    }
+
+    // printNumber.accept(x) outputs "x", where x is an integer.
+    public void number(IntConsumer printNumber) throws InterruptedException {
+        for(int i = 1; i <= n; i++) {
+            try {
+                numberSemaphore.acquire();
+                if(i%3 == 0 && i%5 == 0){
+                    fizzBuzzSemaphore.release();
+                } else if(i%3 == 0) {
+                    fizzSemaphore.release();
+                } else if(i%5 == 0) {
+                    buzzSemaphore.release();
+                } else {
+                    printNumber.accept(i);
+                    numberSemaphore.release();
+                }
+            } catch(InterruptedException e) {
+                Thread.currentThread().interrupt();
+            }
+        }
+        numberSemaphore.acquire();
+        isEnded = true;
+        fizzSemaphore.release();
+        buzzSemaphore.release();
+        fizzBuzzSemaphore.release();
+    }
+    static void main() {
+        FizzBuzz fizzBuzz = new FizzBuzz(15);
+        new Thread(() -> {
+            try {
+                fizzBuzz.fizz(() -> System.out.println("fizz"));
+            } catch (InterruptedException e) {
+                Thread.currentThread().interrupt();
+            }
+        }).start();
+
+        new Thread(() -> {
+            try {
+                fizzBuzz.buzz(() -> System.out.println("buzz"));
+            } catch (InterruptedException e) {
+                Thread.currentThread().interrupt();
+            }
+        }).start();
+
+        new Thread(() -> {
+            try {
+                fizzBuzz.fizzbuzz(() -> System.out.println("fizzbuzz"));
+            } catch (InterruptedException e) {
+                Thread.currentThread().interrupt();
+            }
+        }).start();
+
+        new Thread(() -> {
+            try {
+                fizzBuzz.number(System.out::println);
+            } catch (InterruptedException e) {
+                Thread.currentThread().interrupt();
+            }
+        }).start();
+    }
+}
